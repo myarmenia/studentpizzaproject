d785a79ecb81d6496f3d097fcc58ebb6b7af29a2
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-01-26 18:35:01 +0400

    Swagger Added

--- Router/PizzaRouter.js
+++ Router/PizzaRouter.js
@@ -16,6 +16,22 @@ const pizzaRouter = Router();
  *    get:
  *      summary: Get all pizzas
  *      tags: [Pizza]
+ *      parameters:
+ *        - in: query
+ *          name: sort
+ *          schema:
+ *            type: string
+ *          description: Sort pizzas by a specific attribute (e.g., "alp", "price", "rating").
+ *        - in: query
+ *          name: order
+ *          schema:
+ *            type: string
+ *          description: Order of sorting (e.g., "asc" for ascending, "desc" for descending).
+ *        - in: query
+ *          name: filter
+ *          schema:
+ *            type: string
+ *          description: Filter pizzas based on a specific criteria.
  *      responses:
  *        200:
  *          description: Success
