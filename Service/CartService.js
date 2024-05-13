f3efe63d50290c9ca413aadd43786c4b5b6b26bc
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-05-13 12:05:05 +0400

    test logs

--- Service/CartService.js
+++ Service/CartService.js
@@ -25,7 +25,7 @@ const cartService = {
   addToCart: async (pizzaId, type, size) => {
     console.log("req.body service =>", pizzaId, type, size);
     if (!pizzaId || !type || !size) return { mesage: `Поля не предоставлены` };
-
+    console.log("aaaa");
     if (!Types.ObjectId.isValid(pizzaId))
       return {
         mesage: `Предоставленный ID не соответствует правильному синтаксису ID.`,
